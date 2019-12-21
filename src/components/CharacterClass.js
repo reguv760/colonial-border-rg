f837100f557eeb13c4e82b39371823e0ac6b98a9
reguv760 <reguv760@gmail.com>
2019-12-20 20:33:32 -0800

    add mouseOver/mouseOut functionality

--- src/components/CharacterClass.js
+++ src/components/CharacterClass.js
@@ -1,19 +1,51 @@
 import React, { Component } from "react";
+
 import FrameImg from "./../assets/class-select-frame.png";
+import AnimatedBorder from "./../assets/border-anim.png";
 
 class CharacterClass extends Component {
+  state = {
+    animatedImageLoaded: false
+  };
+
+  MouseOverHandler = () => {
+    this.setState(state => ({
+      animatedImageLoaded: !state.animatedImageLoaded
+    }));
+  };
+
+  getImageName = () =>
+    this.state.animatedImageLoaded ? "afterImage" : "beforeImage";
+  // MouseOverHandler = e => {
+  //   e.target.src = AnimatedBorder;
+  // };
+
+  // MouseOutHandler = e => {
+  //   e.target.src = FrameImg;
+  // };
+
   render() {
     const CharacterType = this.props.characterType;
     const CharacterWeapon = this.props.characterWeapon;
     const CharacterPotion = this.props.characterPotion;
     const CharacterImg = this.props.characterImage;
 
+    const currentImage = {
+      beforeImage: FrameImg,
+      afterImage: AnimatedBorder
+    };
+
+    const imageName = this.getImageName();
+
     return (
       <div className='character-item'>
-        <div className='button'>
+        <div
+          className='button'
+          onMouseOver={this.MouseOverHandler}
+          onMouseOut={this.MouseOverHandler}
+        >
           <div className='buttonContainer'>
-            <img className='border' src={FrameImg} alt='Frame' />
-
+            <img className='border' src={currentImage[imageName]} alt='Frame' />
             <div className='character_text'>
               <p>
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit.
@@ -21,11 +53,9 @@ class CharacterClass extends Component {
                 nam.
               </p>
             </div>
-
             <div className='namePlate_container'>
               <h4>{CharacterType}</h4>
             </div>
-
             <ul className='items_container'>
               <li>
                 <h4>{CharacterWeapon}</h4>
@@ -34,7 +64,6 @@ class CharacterClass extends Component {
                 <h4>{CharacterPotion}</h4>
               </li>
             </ul>
-
             <div className='characterContainer'>
               <img
                 src={CharacterImg}
